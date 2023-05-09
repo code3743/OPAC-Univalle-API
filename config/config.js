e109b5a953a31d89e09ba1237317c141e0b8a836
code3743 <jota.lopez@correounivalle.edu.co>
2023-05-09 16:16:45 -0500

    Inyeccion de scripts de models

--- config/config.js
+++ config/config.js
@@ -1,11 +1,25 @@
 const { chromium} = require('playwright-chromium');
+const generarScriptModels = require('./scripts.models');
 
 const options = {
   headless: true, // Ejecutar sin interfaz gráfica
   args: ['--no-sandbox'] // Parámetros de Chromium
 };
 
-const browser = async () => await chromium.launch(options);
+
+
+
+const browser = async () =>{
+  const navegador = await chromium.launch(options);
+  const context = await navegador.newContext();
+generarScriptModels.forEach(async(script)=>{
+  await context.addInitScript({
+    content: script
+  })
+});
+return context;
+}
+  
 
 module.exports = {
   browser
