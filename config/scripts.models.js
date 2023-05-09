96e09300acc1def02e36a78ef669899a7ea65434
code3743 <jota.lopez@correounivalle.edu.co>
2023-05-09 16:16:10 -0500

    Lectura de las clases en Models para inyectar al contexto del navegador

---
+++ config/scripts.models.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const rutasModels = [
+    path.join(__dirname,'..', 'models', 'libro.prestamo.model.js'),
+    path.join(__dirname,'..', 'models', 'libro.busqueda.model.js'),
+    path.join(__dirname,'..', 'models', 'localizacion.ejemplar.model.js'),
+];
+
+const generarScriptModels = rutasModels.map(ruta => fs.readFileSync(ruta, 'utf-8').split('module.exports')[0]);
+
+
+module.exports = generarScriptModels;
