2a9e1ef95a12e24b9db7fc92e77154439028e114
code3743 <jota.lopez@correounivalle.edu.co>
2023-05-11 22:48:11 -0500

    Variables de entorno

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const BrowserSingleton = require('./config/config');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
