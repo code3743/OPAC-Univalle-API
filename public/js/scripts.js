92cdd1474c5a551d0e6025bc972ad43984d48fd8
Code3743 <jota.lopez@correounivalle.edu.co>
2023-06-11 17:23:55 -0500

    Favicon agregado

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,11 @@
-window.addEventListener('DOMContentLoaded', () => {
-    fetch('/logs')
-      .then(response => response.text())
-      .then(logs => {
-        const logContent = document.getElementById('log-content');
-        logContent.textContent = logs;
-      })
-      .catch(error => {
-        console.error('Error al cargar los registros de log:', error);
-      });
-  });
+window.addEventListener("DOMContentLoaded", () => {
+  fetch("/logs")
+    .then((response) => response.text())
+    .then((logs) => {
+      const logContent = document.getElementById("log-content");
+      logContent.textContent = logs;
+    })
+    .catch((error) => {
+      console.error("Error al cargar los registros de log:", error);
+    });
+});
