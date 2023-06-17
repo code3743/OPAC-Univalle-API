6ff90b48a444ae212e6b102483c81371c443df62
Code3743 <jota.lopez@correounivalle.edu.co>
2023-06-17 14:38:13 -0500

    conexion a db

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config();
+const express = require('express');
+const DbLocal = require('./data/db.local');
 
 const app = express();
+const db = new DbLocal();
 const port = process.env.PORT || 3000;
 const logsPath = path.join(__dirname, 'logs', 'logs.log');
 
-app.use(express.static('public'));
-app.use(express.json());
+db.init();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
 app.use('/api', require('./routers/opac.router'));
 app.get('/logs', (req, res) =>res.sendFile(logsPath));
+
 app.listen(port, () => { console.log('Corriendo en el puerto:', port)});
