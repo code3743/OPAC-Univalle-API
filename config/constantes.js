04135f5fb2cc3421f349d810617049f055f24141
Code3743 <jota.lopez@correounivalle.edu.co>
2023-06-18 13:23:26 -0500

    Nuevas constantes, url de la biblioteca

--- config/constantes.js
+++ config/constantes.js
@@ -1,5 +1,8 @@
 module.exports = {
     URL: 'http://opac.univalle.edu.co/cgi-olib/',
+    URL_BIBLIOTECA: 'https://biblioteca.univalle.edu.co/',
+    LISTADO_DB: '/2-uncategorised/624-lista-alfabetica-de-bases-de-datos-de-libros-y-revistas',
+    URL_BUSCADOR: 'https://apis.ebsco.com/public/rma-pfapi/v1/pf/pfaccount/s6493750.main.pfui/publications',
     INPUT_LOGIN: 'input[id="login1"]',
     LOGIN_SELECTOR: '#login1',
     BOTTON_LOGIN: 'input[value=" Ingresar "]',
